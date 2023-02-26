9bc390e138a12ccc5443707525afb23ff1cdd885
Curcudel Teodor <teocurcudel@gmail.com>
2023-02-26 19:22:58 +0200

    am adaugat test

---
+++ test/test_github/src/test_github/Test_github.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package test_github;
+
+/**
+ *
+ * @author Teo
+ */
+public class Test_github {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Aceasta e o proba.");
+    }
+    
+}
